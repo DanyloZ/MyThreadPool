1018dee52386f3b3839b8a0eb57eb3c05c7ebb8d
DanyloZ <d.zrazhevskiy@gmail.com>
2017-04-07 00:57:06 +0300

    with debug souts

--- src/com/danylo/mythreadpool/MyThreadPool.java
+++ src/com/danylo/mythreadpool/MyThreadPool.java
@@ -1,86 +1,80 @@
 package com.danylo.mythreadpool;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class MyThreadPool implements Executor {
-    private List<Worker> threads = new ArrayList<>();
+    private Map<Worker, Thread> workers = new HashMap<>();
     private BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
-    private boolean isStopped;
+    private volatile boolean isStopped;
+    private volatile boolean isStoppedNow;
 
-    private class Worker extends Thread {
+    private class Worker implements Runnable {
         private boolean isWaitingForTask;
+
         @Override
         public void run() {
-            while(!isStopped && !tasks.isEmpty()) {
-                try {
+            try {
+                while (true) {
                     isWaitingForTask = true;
+                    System.out.println("available tasks:" + tasks.size());
                     Runnable task = tasks.take();
                     isWaitingForTask = false;
                     task.run();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    if (isStoppedNow || (isStopped && tasks.isEmpty())) {
+                        break;
+                    }
                 }
+            } catch (InterruptedException e) {
+               //NOP
             }
+            System.out.println("Worker is stopped");
         }
     }
 
     public MyThreadPool(int size) {
         for (int i = 0; i < size; i++) {
             Worker worker = new Worker();
-            worker.start();
-            threads.add(worker);
+            Thread thread = new Thread(worker);
+            thread.start();
+            System.out.println("Worker " + i + " started");
+            workers.put(worker, thread);
         }
     }
 
     @Override
     public void execute(Runnable command) {
+        if (isStopped) {
+            throw new RuntimeException("ThreadPool is stopped");
+        }
         tasks.add(command);
     }
 
     public void shutdown() {
         isStopped = true;
-        stopWaitingWorkers();
+        System.out.println("shutting down");
+        for (Map.Entry<Worker, Thread> entry : workers.entrySet()) {
+            if (entry.getKey().isWaitingForTask) {
+                entry.getValue().interrupt();
+            }
+        }
     }
 
     public List<Runnable> shutdownNow() {
-        isStopped = true;
-        stopWaitingWorkers();
+        isStoppedNow = true;
+        System.out.println("shutting down now");
+        for (Thread thread : workers.values()) {
+            thread.interrupt();
+        }
         List<Runnable> commands = new ArrayList<>();
         tasks.drainTo(commands);
         return commands;
     }
 
-    private void stopWaitingWorkers() {
-        for (Worker worker : threads) {
-            if (worker.isWaitingForTask) {
-                worker.interrupt();
-            }
-        }
-    }
 
-    public static void main(String[] args) throws InterruptedException {
-        MyThreadPool pool = new MyThreadPool(3);
-        for (int i = 1; i < 11; i++) {
-            final int taskNum = i;
-            pool.execute(new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println("Task" + taskNum + "started");
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Task" + taskNum + "ended");
-                }
-            });
-        }
-        Thread.sleep(4000);
-        List<Runnable> unexecutedTasks = pool.shutdownNow();
-        System.out.println("unexecuted tasks quantity: " + unexecutedTasks.size());
-    }
 }
---
+++ src/com/danylo/mythreadpool/Test.java
@@ -0,0 +1,31 @@
+package com.danylo.mythreadpool;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        MyThreadPool pool = new MyThreadPool(3);
+        Thread.sleep(500);
+        for (int i = 1; i < 11; i++) {
+            final int taskNum = i;
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("Task " + taskNum + " started");
+                        Thread.sleep(2000);
+                        System.out.println("Task " + taskNum + " ended");
+                    } catch (InterruptedException e) {
+                        System.out.println("Task " + taskNum + " is interrupted");
+                    }
+                }
+            });
+            System.out.println("tast " + taskNum + " created");
+            Thread.sleep(100);
+        }
+        Thread.sleep(3000);
+//        pool.shutdown();
+        List<Runnable> unexecutedTasks = pool.shutdownNow();
+        System.out.println("unexecuted tasks quantity: " + unexecutedTasks.size());
+    }
+}
